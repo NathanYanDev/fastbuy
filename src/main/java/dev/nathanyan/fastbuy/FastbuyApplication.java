4a2d01adbfbd44ade8e7e9e5e4abd2b0151e176d
Nathan Yan <nathan_yan_alves@hotmail.com>
2026-02-19 21:18:45 -0300

    initial commit

---
+++ src/main/java/dev/nathanyan/fastbuy/FastbuyApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanyan.fastbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanyan/fastbuy/FastbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanyan.fastbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
